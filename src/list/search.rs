4f2fe71d8105bb05c6267d6fcd7ee7bcff467c04
Alex Solis <git@alexsol.is>
2025-09-20 00:12:44 -0400

    Add 'add' command

--- src/list/search.rs
+++ src/list/search.rs
@@ -1,4 +1,5 @@
 use crate::list;
+use crate::search_paths;
 
 pub fn complete_item(list: &mut Vec<list::Item>, prefix: String) -> bool{
     for (i, item) in list.clone().into_iter().enumerate() {
@@ -30,4 +31,38 @@ pub fn toggle_item(list: &mut Vec<list::Item>, prefix: String) -> bool {
     }
 
     false
+}
+
+pub fn add_item(list: &mut Vec<list::Item>, new_item: list::Item, prefix: Vec<&str>) -> bool{
+    if prefix.len() == 0 {
+        for item in list.clone() {
+            if item.name.starts_with(&new_item.name) {
+                println!("Item already exits");
+                return false;
+            }
+        }
+        list.push(new_item);
+        return true;
+    } else {
+        for (i, item) in list.clone().into_iter().enumerate() {
+            if item.name.starts_with(prefix[0]) {
+                return add_item(&mut list[i].items, new_item, prefix[1..].to_vec());
+            }
+        }
+    };
+
+    false
+}
+
+pub fn get_list(name: String) -> Result<list::List, String> {
+    let paths = search_paths::search_up();
+
+    for path in paths {
+        let list = list::parse_list(path);
+        if list.name == name {
+            return Ok(list)
+        }
+    }
+
+    Err(format!("No list called '{name}'"))
 }
--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     match &cmd[..] {
         "new" => new(&mut std::env::args().skip(2)),
         "list" =>  list(std::env::args().skip(2)),
-        "add" =>  print!("add"),
+        "add" => add(&mut std::env::args().skip(2)),
         "remove" => println!("remove"),
         "complete" => complete(&mut std::env::args().skip(2)),
         "toggle" => toggle(&mut std::env::args().skip(2)),
@@ -33,6 +33,36 @@ fn list (_args: std::iter::Skip<std::env::Args>) {
 
     for path in paths {
         println!("{}", list::format::format_list(list::parse_list(path.clone()), path));
+    }
+}
+
+fn add (args: &mut std::iter::Skip<std::env::Args>) {
+    let list_name_input = args.next().unwrap();
+    let item_path = args.next().unwrap();
+
+    let list_name = if list_name_input.starts_with("#") {list_name_input[1..].to_string()} else {list_name_input};
+
+    let item_path_components = item_path.split("/");
+
+    let parents = item_path_components.clone().take(item_path_components.clone().count() - 1);
+    let item_name = item_path_components.last().unwrap_or("Unnamed Item");
+
+    let new_item = list::Item {
+        name: item_name.to_string(),
+        priority: 0,
+        date: "".to_string(),
+        completed: false,
+        items: vec![]
+    };
+
+    let mut list = list::search::get_list(list_name.clone()).unwrap();
+
+    let result = list::search::add_item(&mut list.items, new_item, parents.collect());
+
+    if result {
+        list::save::save_list(list);
+
+        println!("Added {item_path} to #{list_name}");
     }
 }
 
