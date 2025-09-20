c85c1dbee48b8ca6fe85ce9faee5a7a334d6a72c
Alex Solis <git@alexsol.is>
2025-09-20 11:17:23 -0400

    Added packge building to flake.nix

--- flake.nix
+++ flake.nix
@@ -3,26 +3,75 @@
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
+
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    crane.url = "github:ipetkov/crane";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, crane, flake-utils }:
     flake-utils.lib.eachDefaultSystem
       (system:
-        let pkgs = nixpkgs.legacyPackages.${system}; in
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+
+          craneLib = crane.mkLib pkgs;
+
+          # Common arguments can be set here to avoid repeating them later
+          # Note: changes here will rebuild all dependency crates
+          commonArgs = {
+            src = craneLib.cleanCargoSource ./.;
+            strictDeps = true;
+
+            buildInputs = [
+              # Add additional build inputs here
+            ]
+            ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+              # Additional darwin specific inputs can be set here
+              pkgs.libiconv
+            ];
+          };
+
+          todo-rs = craneLib.buildPackage (
+            commonArgs
+            // {
+              cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+              # Additional environment variables or build phases/hooks can be set
+              # here *without* rebuilding all dependency crates
+              # MY_CUSTOM_VAR = "some value";
+            }
+          );
+        in
         {
-          devShells.default = pkgs.mkShell
-            {
-              packages = with pkgs; [
-                rustc
-                cargo
-              ];
-              shellHook = ''
-                rustc -V
-                cargo -V
-                export PATH=$PATH:./target/debug/:./target/release/
-                echo "Added 'todo' binary to \$PATH"
-              '';
-            };
+          checks = {
+            inherit todo-rs;
+          };
+
+          packages.default = todo-rs;
+
+          apps.default = flake-utils.lib.mkApp {
+            drv = todo-rs;
+          };
+
+          devShells.default = craneLib.devShell {
+            # Inherit inputs from checks.
+            checks = self.checks.${system};
+
+            # Additional dev-shell environment variables can be set directly
+            # MY_CUSTOM_DEVELOPMENT_VAR = "something else";
+
+            # Extra inputs can be added here; cargo and rustc are provided by default.
+            packages = with pkgs; [
+              # pkgs.ripgrep
+            ];
+            shellHook = ''
+              rustc -V
+              cargo -V
+              export PATH=$PATH:./target/debug/:./target/release/
+              echo "Added 'todo' binary to \$PATH"
+            '';
+          };
         }
       );
 }
